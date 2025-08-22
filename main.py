aa3c6b447b53b6281f494a6d4bc9204970a6520b
João Pedro Alves <jpafa13@gmail.com>
2025-08-22 15:29:53 -0300

    Acessando camêra e identificando rostos

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 from flask import Flask, request
+import cv2
+import mediapipe as mp
+
+# Configurando OpenCV e Mediapipe
+webcam = cv2.VideoCapture(0)
+solucao = mp.solutions.face_detection
+reconhecer = solucao.FaceDetection()
+desenho = mp.solutions.drawing_utils
+
+while True:
+    # Verificando se a camêra está funcionando
+    verificar, imagem = webcam.read()
+    if not verificar:
+        break
+    
+    # Reconhecendo rostos na camêra
+    lista_de_rostos = reconhecer.process(imagem)
+
+    # Desenhando na imagem da camêra
+    if lista_de_rostos.detections:
+        for rosto in lista_de_rostos.detections:
+            desenho.draw_detection(imagem, rosto)
+    cv2.imshow('Teste de camêra', imagem)
+
+    # Código para fechar a camêra no ESC
+    if cv2.waitKey(5) == 27:
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
